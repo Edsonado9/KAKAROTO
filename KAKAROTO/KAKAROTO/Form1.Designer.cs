fc4632ee121784467715f9d7dddb2b9a272e4616
Edsonado9 <47904705+Edsonado9@users.noreply.github.com>
2019-02-26 22:12:34 -0600

    JIREN

--- KAKAROTO/KAKAROTO/Form1.Designer.cs
+++ KAKAROTO/KAKAROTO/Form1.Designer.cs
@@ -35,15 +35,15 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(357, 87);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(268, 71);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(337, 197);
+            this.button1.Size = new System.Drawing.Size(253, 160);
             this.button1.TabIndex = 0;
             this.button1.Text = "KAKAROTO";
             this.button1.UseVisualStyleBackColor = true;
@@ -52,10 +52,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(783, 107);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(587, 87);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(133, 46);
+            this.label1.Size = new System.Drawing.Size(109, 37);
             this.label1.TabIndex = 1;
             this.label1.Text = "HOLA";
             // 
@@ -63,10 +62,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(783, 197);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(587, 160);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 46);
+            this.label2.Size = new System.Drawing.Size(88, 37);
             this.label2.TabIndex = 2;
             this.label2.Text = "SOY";
             // 
@@ -74,10 +72,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(783, 290);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(587, 236);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(141, 46);
+            this.label3.Size = new System.Drawing.Size(115, 37);
             this.label3.TabIndex = 3;
             this.label3.Text = "GOKU";
             // 
@@ -85,10 +82,9 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(783, 357);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(587, 290);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(141, 46);
+            this.label4.Size = new System.Drawing.Size(115, 37);
             this.label4.TabIndex = 4;
             this.label4.Text = "GOKU";
             // 
@@ -96,27 +92,38 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(463, 311);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(347, 253);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(141, 46);
+            this.label5.Size = new System.Drawing.Size(115, 37);
             this.label5.TabIndex = 5;
             this.label5.Text = "GOKU";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(383, 395);
+            this.button2.Location = new System.Drawing.Point(287, 321);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(311, 147);
+            this.button2.Size = new System.Drawing.Size(233, 119);
             this.button2.TabIndex = 6;
             this.button2.Text = "VEGETTA";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(560, 338);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(239, 37);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "JIREN PERRO";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -124,7 +131,6 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -141,6 +147,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
     }
 }
 
